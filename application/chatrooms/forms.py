47eb14bb7419d06529c469d2220cb34bb1011de7
luismasuelli <luis@triplemint.com>
2020-09-24 21:17:20 -0500

    Adding email field to form

--- application/chatrooms/forms.py
+++ application/chatrooms/forms.py
@@ -1,5 +1,6 @@
+from django.core.exceptions import ValidationError
 from django.forms import Form
-from django.forms.fields import CharField
+from django.forms.fields import CharField, EmailField
 from django.forms.widgets import PasswordInput
 from django.contrib.auth.forms import UsernameField
 
@@ -19,5 +20,12 @@ class RegisterForm(Form):
     """
 
     username = UsernameField()
+    email = EmailField()
     password = CharField(widget=PasswordInput)
     password_confirm = CharField(widget=PasswordInput)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        if cleaned_data['password'] != cleaned_data['password_confirm']:
+            raise ValidationError("Passwords do not match")
+        return cleaned_data
