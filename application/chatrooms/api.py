e29b0fb538f4b185e310680bbdb23bfdf564e317
luismasuelli <luis@triplemint.com>
2020-09-25 09:50:51 -0500

    Changing serializers and logout url.

---
+++ application/chatrooms/api.py
@@ -0,0 +1,45 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from rest_framework.authtoken.models import Token
+from rest_framework.generics import CreateAPIView
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import UserCreateSerializer, UserLoginSerializer
+from .signals import session_destroyed
+
+
+class UserCreateView(CreateAPIView):
+    """
+    Registers a user. The iser is immediately available
+      for log-in.
+    """
+
+    serializer_class = UserCreateSerializer
+
+
+class UserLoginView(APIView):
+    """
+    Performs a log-in for a user.
+    """
+
+    def post(self, request, *args, **kwargs):
+        serializer = UserLoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token = Token.objects.get_or_create(user=user)
+        # Everything is fine in this login process.
+        # Let's return the key.
+        return Response({
+            'token': token
+        }, status=200)
+
+
+class UserLogoutView(APIView, LoginRequiredMixin):
+    """
+    Performs a log-out for a user.
+    """
+
+    def post(self, request, *args, **kwargs):
+        token = Token.objects.get(user=request.user)
+        session_destroyed.send(sender=token)
+        token.delete()
+        return Response('', status=204)
--- application/chatrooms/urls.py
+++ application/chatrooms/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from . import views
+from . import views, api
 
 app_name = 'chatrooms'
 urlpatterns = [
     path('', views.welcome, name='welcome'),
     path('goodbye', views.goodbye, name='goodbye'),
-    path('main', views.main, name='main')
+    path('main', views.main, name='main'),
+
+    # API endpoints:
+    path('login', api.UserLoginView.as_view()),
+    path('register', api.UserCreateView.as_view()),
+    path('logout', api.UserLogoutView)
 ]
