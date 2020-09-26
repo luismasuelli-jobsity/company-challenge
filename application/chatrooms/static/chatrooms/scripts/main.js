34ad597d785db5a6c6f1049038b52913d6840fd7
luismasuelli <luis@triplemint.com>
2020-09-26 11:18:51 -0500

    Adding the serializeObject function

--- application/chatrooms/static/chatrooms/scripts/main.js
+++ application/chatrooms/static/chatrooms/scripts/main.js
@@ -1,4 +1,20 @@
 (function($) {
+    $.fn.serializeObject = $.fn.serializeObject || function()  {
+        let o = {};
+        let a = this.serializeArray();
+        $.each(a, function() {
+            if (o[this.name]) {
+                if (!o[this.name].push) {
+                    o[this.name] = [o[this.name]];
+                }
+                o[this.name].push(this.value || '');
+            } else {
+                o[this.name] = this.value || '';
+            }
+        });
+        return o;
+    };
+
     window.Chat = {
         // Token-related utility functions start here.
 
