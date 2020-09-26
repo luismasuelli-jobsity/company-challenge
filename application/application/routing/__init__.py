db81143a5a0f57c5e6f7c4a3eb8a55b6c2ac4922
luismasuelli <luis@triplemint.com>
2020-09-26 17:42:39 -0500

    Using the token middleware

--- application/application/routing/__init__.py
+++ application/application/routing/__init__.py
@@ -1,10 +1,10 @@
-from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+from application.routing.auth import TokenAuthMiddlewareStack
 from chatrooms.routing import websocket_urlpatterns
 
 channels_router = ProtocolTypeRouter({
     # (http->django views is added by default)
-    'websocket': AuthMiddlewareStack(
+    'websocket': TokenAuthMiddlewareStack(
         URLRouter(
             websocket_urlpatterns
         )
