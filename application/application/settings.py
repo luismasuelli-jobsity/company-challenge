948da26275364dd04bf391ec4c46389db27686d2
luismasuelli <luis@triplemint.com>
2020-09-26 12:25:48 -0500

    Adding json renderers for rest framework

--- application/application/settings.py
+++ application/application/settings.py
@@ -56,6 +56,9 @@ MIDDLEWARE = [
 ]
 
 REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
